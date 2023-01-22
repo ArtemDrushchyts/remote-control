12682f93141f50c1461599d5f73a917ec281744e
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2023-01-22 12:08:58 +0300

    feat: add ws server

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 import { httpServer } from "./src/http_server/index";
 import { mouse } from "@nut-tree/nut-js";
+import { WebSocketServer } from "ws";
+import * as dotenv from "dotenv";
 
-const HTTP_PORT = 8181;
+dotenv.config();
+
+const HTTP_PORT = process.env.HTTP_PORT;
+const WS_PORT = Number(process.env.WS_PORT);
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wsServer = new WebSocketServer({port: WS_PORT})
+
+console.log(`Start websocket server on the ${WS_PORT} port!`);
+
+wsServer.on('connection', () => {
+    console.log('Connect')
+})
+
+process.on("SIGNINT", () => {
+    wsServer.close();
+    process.exit(0);
+})
